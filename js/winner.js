d4b65478b2051a8238835adcdb3a6c548f7d6058
Marcos <marcosmarquitos132@gmail.com>
2023-02-14 22:57:19 +0100

    nombre player en pantalla winner (prueba 1)

--- js/winner.js
+++ js/winner.js
@@ -1,1 +1,7 @@
+let ganador1 = sessionStorage.getItem("player-1");
+document.getElementById("Playerwin").innerHTML = `<p>${ganador1}</p>`;
+
+let ganador2 = sessionStorage.getItem("player-2");
+document.getElementById("Playerwin").innerHTML = `<p>${ganador2}</p>`;
+
 sessionStorage.clear();
