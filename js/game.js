8a749005e50292f4da66cb25fbdbd1ddb3607555
Marcos <marcosmarquitos132@gmail.com>
2023-02-15 10:14:47 +0100

    eliminado de turnos en pag game.html

--- js/game.js
+++ js/game.js
@@ -37,8 +37,8 @@ tablero.map(
 
 //combinacion ganadora
 let combinacionGanadora=[
-    [0, 1, 2], //posicion 0 
-    [3, 4, 5], //posicion 1
+    [0, 1, 2], 
+    [3, 4, 5], 
     [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
