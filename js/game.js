5e71ccb1c65f167f72b00f210e87c658d819d472
Marcos <marcosmarquitos132@gmail.com>
2023-02-20 09:26:35 +0100

    simplificado codigo

--- js/game.js
+++ js/game.js
@@ -59,71 +59,28 @@ let combinacionGanadora=[
     [2, 4, 6]
 ];
 
-
+//Combinacion ganadora
 const comprueboGanador = () => {
 
-//primera combinacion ganadora 1a fila 
-if ((miTablero[combinacionGanadora[0][0]] === "X") && (miTablero[combinacionGanadora[0][1]] === "X") && (miTablero[combinacionGanadora[0][2]] === "X")){
+if((miTablero[combinacionGanadora[0][0]] === "X") && (miTablero[combinacionGanadora[0][1]] === "X") && (miTablero[combinacionGanadora[0][2]] === "X") ||
+(miTablero[combinacionGanadora[1][0]] === "X") && (miTablero[combinacionGanadora[1][1]] === "X") && (miTablero[combinacionGanadora[1][2]] === "X") ||
+(miTablero[combinacionGanadora[2][0]] === "X") && (miTablero[combinacionGanadora[2][1]] === "X") && (miTablero[combinacionGanadora[2][2]] === "X") ||
+(miTablero[combinacionGanadora[3][0]] === "X") && (miTablero[combinacionGanadora[3][1]] === "X") && (miTablero[combinacionGanadora[3][2]] === "X") ||
+(miTablero[combinacionGanadora[4][0]] === "X") && (miTablero[combinacionGanadora[4][1]] === "X") && (miTablero[combinacionGanadora[4][2]] === "X") ||
+(miTablero[combinacionGanadora[5][0]] === "X") && (miTablero[combinacionGanadora[5][1]] === "X") && (miTablero[combinacionGanadora[5][2]] === "X") ||
+(miTablero[combinacionGanadora[6][0]] === "X") && (miTablero[combinacionGanadora[6][1]] === "X") && (miTablero[combinacionGanadora[6][2]] === "X") ||
+(miTablero[combinacionGanadora[7][0]] === "X") && (miTablero[combinacionGanadora[7][1]] === "X") && (miTablero[combinacionGanadora[7][2]] === "X")){
     sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player1").innerHTML));
     window.location.href= "../pages/winner.html"
-//primera combinacion ganadora 1a fila 
-} else if  ((miTablero[combinacionGanadora[0][0]] === "O") && (miTablero[combinacionGanadora[0][1]] === "O") && (miTablero[combinacionGanadora[0][2]] === "O")){
+} else if((miTablero[combinacionGanadora[0][0]] === "O") && (miTablero[combinacionGanadora[0][1]] === "O") && (miTablero[combinacionGanadora[0][2]] === "O") ||
+(miTablero[combinacionGanadora[1][0]] === "O") && (miTablero[combinacionGanadora[1][1]] === "O") && (miTablero[combinacionGanadora[1][2]] === "O") ||
+(miTablero[combinacionGanadora[2][0]] === "O") && (miTablero[combinacionGanadora[2][1]] === "O") && (miTablero[combinacionGanadora[2][2]] === "O") ||
+(miTablero[combinacionGanadora[3][0]] === "O") && (miTablero[combinacionGanadora[3][1]] === "O") && (miTablero[combinacionGanadora[3][2]] === "O") ||
+(miTablero[combinacionGanadora[4][0]] === "O") && (miTablero[combinacionGanadora[4][1]] === "O") && (miTablero[combinacionGanadora[4][2]] === "O") ||
+(miTablero[combinacionGanadora[5][0]] === "O") && (miTablero[combinacionGanadora[5][1]] === "O") && (miTablero[combinacionGanadora[5][2]] === "O") ||
+(miTablero[combinacionGanadora[6][0]] === "O") && (miTablero[combinacionGanadora[6][1]] === "O") && (miTablero[combinacionGanadora[6][2]] === "O") ||
+(miTablero[combinacionGanadora[7][0]] === "O") && (miTablero[combinacionGanadora[7][1]] === "O") && (miTablero[combinacionGanadora[7][2]] === "O")){
     sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player2").innerHTML));
     window.location.href= "../pages/winner2.html"
-//Segunda combinacion ganadora 2a fila
-} else if  ((miTablero[combinacionGanadora[1][0]] === "X") && (miTablero[combinacionGanadora[1][1]] === "X") && (miTablero[combinacionGanadora[1][2]] === "X")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player1").innerHTML));
-    window.location.href= "../pages/winner.html"
-//Segunda combinacion ganadora 2a fila
-} else if  ((miTablero[combinacionGanadora[1][0]] === "O") && (miTablero[combinacionGanadora[1][1]] === "O") && (miTablero[combinacionGanadora[1][2]] === "O")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player2").innerHTML));
-    window.location.href= "../pages/winner2.html"
-//Tercera combinacion ganadora 3a fila
-} else if  ((miTablero[combinacionGanadora[2][0]] === "X") && (miTablero[combinacionGanadora[2][1]] === "X") && (miTablero[combinacionGanadora[2][2]] === "X")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player1").innerHTML));
-    window.location.href= "../pages/winner.html"
-//Tercera combinacion ganadora 3a fila
-}  else if  ((miTablero[combinacionGanadora[2][0]] === "O") && (miTablero[combinacionGanadora[2][1]] === "O") && (miTablero[combinacionGanadora[2][2]] === "O")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player2").innerHTML));
-    window.location.href= "../pages/winner2.html"
-//Cuarta combinacion ganadora 1a columna
-}  else if  ((miTablero[combinacionGanadora[3][0]] === "X") && (miTablero[combinacionGanadora[3][1]] === "X") && (miTablero[combinacionGanadora[3][2]] === "X")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player1").innerHTML));
-    window.location.href= "../pages/winner.html"
-//Cuarta combinacion ganadora 1a columna
-} else if  ((miTablero[combinacionGanadora[3][0]] === "O") && (miTablero[combinacionGanadora[3][1]] === "O") && (miTablero[combinacionGanadora[3][2]] === "O")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player2").innerHTML));
-    window.location.href= "../pages/winner2.html"
-//Cuarta combinacion ganadora 2a columna
-} else if  ((miTablero[combinacionGanadora[4][0]] === "X") && (miTablero[combinacionGanadora[4][1]] === "X") && (miTablero[combinacionGanadora[4][2]] === "X")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player1").innerHTML));
-    window.location.href= "../pages/winner.html"
-//Cuarta combinacion ganadora 2a columna
-} else if  ((miTablero[combinacionGanadora[4][0]] === "O") && (miTablero[combinacionGanadora[4][1]] === "O") && (miTablero[combinacionGanadora[4][2]] === "O")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player2").innerHTML));
-    window.location.href= "../pages/winner2.html"
-//Quinta combinacion ganadora 3a columna
-} else if  ((miTablero[combinacionGanadora[5][0]] === "X") && (miTablero[combinacionGanadora[5][1]] === "X") && (miTablero[combinacionGanadora[5][2]] === "X")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player1").innerHTML));
-    window.location.href= "../pages/winner.html"
-//Quinta combinacion ganadora 3a columna
-} else if  ((miTablero[combinacionGanadora[5][0]] === "O") && (miTablero[combinacionGanadora[5][1]] === "O") && (miTablero[combinacionGanadora[5][2]] === "O")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player2").innerHTML));
-    window.location.href= "../pages/winner2.html"
-//Sexta combinacion ganadora diagonal 1
-} else if  ((miTablero[combinacionGanadora[6][0]] === "X") && (miTablero[combinacionGanadora[6][1]] === "X") && (miTablero[combinacionGanadora[6][2]] === "X")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player1").innerHTML));
-    window.location.href= "../pages/winner.html"
-//Sexta combinacion ganadora diagonal 1
-} else if  ((miTablero[combinacionGanadora[6][0]] === "O") && (miTablero[combinacionGanadora[6][1]] === "O") && (miTablero[combinacionGanadora[6][2]] === "O")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player2").innerHTML));
-    window.location.href= "../pages/winner2.html"
-//Septima combinacion ganadora diagonal 1
-} else if  ((miTablero[combinacionGanadora[7][0]] === "X") && (miTablero[combinacionGanadora[7][1]] === "X") && (miTablero[combinacionGanadora[7][2]] === "X")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player1").innerHTML));
-    window.location.href= "../pages/winner.html"
-//Septima combinacion ganadora diagonal 1
-} else if  ((miTablero[combinacionGanadora[7][0]] === "O") && (miTablero[combinacionGanadora[7][1]] === "O") && (miTablero[combinacionGanadora[7][2]] === "O")){
-    sessionStorage.setItem("NombreGanador", JSON.stringify(document.getElementById("Player2").innerHTML));
-    window.location.href= "../pages/winner2.html"}
+}
 }
